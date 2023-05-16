deee50b004f501aed2bcec2d0460bcc1ac702d3c
dudngy <118886619+dudngy@users.noreply.github.com>
2023-05-16 12:55:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 
-rand - np.random.normal(1,2,size=20)
-fig, ax - plt.sublots()
-ax.hist(rand, bins-15)
-st.pyplot(fig)
+df = pd.DataFrame(np.random.randn(500,2)/[50,50]+[37.76,-122.4],columns=['lat','lon'])
+st.map(df)
